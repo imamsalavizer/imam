6be900add2b43523b4a174506355d36f882d3626
imamsalavizer <114551601+imamsalavizer@users.noreply.github.com>
2022-09-28 11:17:19 +0700

    Add files via upload

---
+++ imam.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html>
+    <head>
+<title> Tag Gambar dan Buat Tabel</title>
+</head>
+<body style=background-color:grey>
+        <h1><center> BIODATA MAHASISWA </h1>
+<table  align= center border="1" style=background-color:deepskyblue><font size=12>
+<tr>
+<th colspan="3" style=background-color:darkslateblue><center>
+<font size=5 font=calibri color=white> Data Diri </th>
+</tr>
+<tr>
+<td> NIM</td>
+<td> 202520020 </td>
+</tr>
+<tr>
+<td> Nama </td>
+<td> Imam Nur Muttaqin </td>
+</tr>
+<td rowspan="10" colspan="3"> <img src="imam.JPEG" height=500 width=400 /> </td>
+</tr>
+<tr>
+<td> Alamat </td>
+<td> Purworejo </td>
+</tr>
+<tr>
+    <tr>
+        <td> Alamat E-mail</td>
+        <td> imamsalavizer177@gmail.com </td>
+    </tr>
+    <tr>
+        <td> Bidang minat </td>
+        <td> Membaca </td>
+    </tr>
+</tr>
+</body>
+</html>
